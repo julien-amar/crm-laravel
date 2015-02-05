a9b46bc463e95a4b46061295895e2c6b6af661b4
julien-amar <julien.amar@live.fr>
2015-02-05 15:19:09 -0800

    Upload excel file

--- src/app/controllers/ImportController.php
+++ src/app/controllers/ImportController.php
@@ -14,6 +14,11 @@ class ImportController extends BaseController {
 	}
 
 	public function postData() {
+		$inputFileName = Input::file('file')->getFilename();
+
+		/** Load $inputFileName to a PHPExcel Object  **/
+		$objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+		
 		return Redirect::to('import/data')
 			->with('message', '<strong>146 results</strong> had been imported.!') // TODO : Translate
 			->with('message-type', 'success');
